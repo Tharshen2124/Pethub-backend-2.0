e18c38b8d151065fd39073f46e26d53e430c7324
Tharshen2124 <tharshensurianbalan@gmail.com>
2024-02-11 19:05:36 +0800

    feat: add testing case for backend

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,16 +4,71 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public $table = "users";
+    public function test_get_users_route(): void
+    {
+        User::factory(3)->create();
+
+        $user = User::create([
+            'full_name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => '12345',
+            'permission_level' => '3',
+            'contact_number' => '0106673148',
+            'description' => 'nothing',
+            'image' => 'unce.png',
+            'user_status' => 'approved'
+        ]);
+
+        $token = $user->createToken('test-token')->plainTextToken;
+        
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->getJson('/api/v1/admin/user');
+        
+        $user = [
+            "user" => [
+                [
+                    "user_id",
+                    "full_name",
+                    "email",
+                    "permission_level",
+                    "image",
+                    "description",
+                    "contact_number",
+                    "deposit_range",
+                    "service_type",
+                    "opening_hour",
+                    "closing_hour",
+                    "bank_name",
+                    "beneficiary_acc_number",
+                    "beneficiary_name",
+                    "qr_code_image",
+                    "user_status",
+                    "created_at",
+                    "updated_at"
+                ]
+            ]
+        ];
+        $response->dump();
+        // $menu2 = [
+        //     "message" => 'success',
+        // ];
+
+        // use it for dynamic data and if you have a structure
+        $response->assertJsonStructure($user);
+        
+        // use it for hard-coded data
+        //$response->assertJsonFragment($menu2);
     }
 }

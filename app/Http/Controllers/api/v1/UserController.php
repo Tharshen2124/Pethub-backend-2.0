684e1bcb90ba4cda4c96ba7a5685a54c286ddc27
Tharshen2124 <tharshensurianbalan@gmail.com>
2024-02-23 00:36:54 +0800

    fix

--- app/Http/Controllers/api/v1/UserController.php
+++ app/Http/Controllers/api/v1/UserController.php
@@ -264,13 +264,13 @@ class UserController extends Controller
     public function editPetOwner(Request $request, string $id)
     {
         $existingUser = User::where('email', $request->email)->first();
-        
-        if($existingUser && $existingUser->user_status !== 'rejected' && $existingUser->user_id !== $id) {
+
+        if($existingUser && $existingUser->user_id != $id  && $existingUser->user_status !== 'rejected' ) {
             return response()->json([
                 'message' => "Unauthorized request, this email has been used already",
             ], 403);
         }
-        if ($existingUser && $existingUser->user_status === 'rejected' && $existingUser->user_id !== $id) {
+        if ($existingUser && $existingUser->user_id != $id && $existingUser->user_status === 'rejected') {
             return response()->json([
                 'message' => "Unauthorized request, this email has been banned",
             ], 403);
@@ -312,12 +312,12 @@ class UserController extends Controller
     {
         $existingUser = User::where('email', $request->email)->first();
         
-        if($existingUser && $existingUser->user_status !== 'rejected') {
+        if($existingUser && $existingUser->user_id != $id  && $existingUser->user_status !== 'rejected' ) {
             return response()->json([
                 'message' => "Unauthorized request, this email has been used already",
             ], 403);
         }
-        if ($existingUser && $existingUser->user_status === 'rejected') {
+        if ($existingUser && $existingUser->user_id != $id && $existingUser->user_status === 'rejected') {
             return response()->json([
                 'message' => "Unauthorized request, this email has been banned",
             ], 403);
